4d6d2c9601a52896a9f5b322a2da00f11c8c7599
CangeMichele <cange.michele@gmail.com>
2024-04-24 21:37:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+function search() {
+    const input = document.getElementById("searchField").value;
+    const url = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${input}`
+    console.log(url);
+    
+    document.getElementById(input).classList.remove('d-none');
+
+    fetch(url)
+        .then(response => response.json())
+
+        .then(data => {
+            const dataAlbums = data.data;
+            console.log(dataAlbums);
+
+            dataAlbums.forEach(album => {
+
+                let card = document.createElement('div');
+                card.classList.add('card');
+
+                let cardTitle = document.createElement('h5');
+                cardTitle.classList.add('card-title');
+                cardTitle.textContent = album['title'];
+
+
+                let imgCard = document.createElement('img');
+                imgCard.setAttribute('src', album['album']['cover_medium']);
+                imgCard.setAttribute('alt', 'copertina album');
+                imgCard.classList.add('card-img-top');
+
+                let cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+
+                let trackList = album['tracklist'];
+                fetch(trackList)
+                .then (list => list.json())
+                console.log(list);
+                
+                
+                
+                let paragrafo = document.createElement('p');
+                paragrafo.classList.add('card-text');
+                paragrafo.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, vel?';
+
+                cardBody.appendChild(paragrafo);
+                card.appendChild(cardTitle);
+                card.appendChild(imgCard);
+                card.appendChild(cardBody);
+
+                document.getElementById(input + 'Section').appendChild(card);
+            });
+
+        })
+
+
+
+
+
+
+
+
+}
+
+
+
